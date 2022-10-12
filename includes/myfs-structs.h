c43e77091822d1eb63c8c4723c469f43629634ac
deco42 <82336539+deco42@users.noreply.github.com>
2022-10-12 11:14:52 +0200

    Update myfs-structs.h
    struct MyFsFileInfo

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -15,5 +15,19 @@
 #define NUM_OPEN_FILES 64
 
 // TODO: Add structures of your file system here
+struct MyFsFileInfo
+{
+    char cName[NAME_LENGTH];    // Name
+    size_t size;                // Data Size
+    unsigned char* data;        // Data
+    __uid_t uid;                // User ID
+    __gid_t gid;                // Gruppen ID 
+    __mode_t mode;              // File mode
+    struct timespec atime;	    // Time of last access.
+    struct timespec mtime;	    // Time of last modification.
+    struct timespec ctime;	    // Time of last status change.
+
+    char* cPath;                // Path to the file
+};
 
 #endif /* myfs_structs_h */
